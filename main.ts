0ca64acde5d76b878994a5bf19bbbe78507eec02
Nathanpasbeaucoup <92890159+Nathanpasbeaucoup@users.noreply.github.com>
2021-11-16 15:39:58 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+let alarme = 0
+basic.forever(function () {
+    if (alarme == 1) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
